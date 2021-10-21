3fab1df8e3bc089d943020628c35a9dccded2bea
snrntpa2z <snrntpa2z@naver.com>
2021-10-21 16:50:35 +0900

    add: fix repo

--- src/component/KakaoMap.js
+++ src/component/KakaoMap.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Map from "./Map"
 import  Search  from './Search'
 
 
 const KakaoMap = () => {
+  const [search, setSearch] = useState('');
+  const [searchArr, setArrSearch] = useState([]); 
+  const addSearch = () =>{
+    let newArr = [...searchArr];
+    newArr.push(search);
+    setArrSearch(newArr);
+    setSearch('');
+    console.log(searchArr);
+  }
   return (
     <div style={{display: "flex", flexDirection: "row", marginTop:"20px", justifyContent:"space-around" }}>
       <Map />
-      <Search />
+      <div style={{width: '100%', display: 'flex', flexDirection: 'column'}}>
+        <Search onClick={addSearch} value={search} setSearch={setSearch} />
+        <div>
+        {searchArr.map((sec) => (
+          <p>{sec}</p>
+        ))} 
+        </div>
+      </div>
     </div>
   )
 }
--- src/component/Search.js
+++ src/component/Search.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {Button, Input} from "semantic-ui-react"
 
-const Search = () => {
+const Search = ({onClick, value, setSearch}) => {
   
   return (
     <div style={{height:"60px", width:"70%", display:"flex", justifyContent: "center"}}>
-      <Input type="text" id ="searchBox" placeholder= "데이트 장소를 검색해주세요." style={{width:"30%"}}></Input>
-      <Button  color='violet' style={{marginLeft:"10px", width:"70px", height:"60px"}}>검색</Button>
+      <Input value={value} onChange={(e) => setSearch(e.target.value)} type="text" id ="searchBox" placeholder= "데이트 장소를 검색해주세요." style={{width:"30%"}}></Input>
+      <Button onClick={onClick} color='violet' style={{marginLeft:"10px", width:"70px", height:"60px"}}>검색</Button>
     </div>
   )
 }
