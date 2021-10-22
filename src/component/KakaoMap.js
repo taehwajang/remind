0a7b17b97b0c83e44bdddc96c80e18573cbade75
taehwajang <61370482+taehwajang@users.noreply.github.com>
2021-10-22 22:52:07 +0900

    css

--- src/component/KakaoMap.js
+++ src/component/KakaoMap.js
@@ -29,9 +29,9 @@ const KakaoMap = () => {
       <Map />
       <div style={{width: '100%', display: 'flex', flexDirection: 'column'}}>
         <Search onClick={addSearch} onClick2={reset} value={search} setSearch={setSearch} />
-        <div style={{border:"1px solid black" , display:"flex", justifyContent:"center" ,flexDirection:"column", marginTop:"20px", width:"60%", marginLeft:"100px",}}>
+        <div style={{border:"1px solid black" , display:"flex", justifyContent:"center" ,flexDirection:"column", marginTop:"20px", width:"60%", marginLeft:"100px",  }}>
         {searchArr.map((sec) => (
-          <p id={searchArr.length -1 }>{sec}<Button onClick={deleteBtn(searchArr.length -1)}>삭제</Button></p>
+          <p id={searchArr.length -1 } style={{marginTop:"5px",marginBottom:"5px"}} >{sec}<Button onClick={deleteBtn(searchArr.length -1)}>삭제</Button></p>
           
         ))} 
           
